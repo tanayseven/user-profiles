ba861916966bca528f68e451ee69f4d611291621
Tanay PrabhuDesai <tanayseven@gmail.com>
2021-02-13 21:27:30 +0530

    Disallow un-used variables

--- src/login.test.tsx
+++ src/login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter, Router } from 'react-router'
+import { Router } from 'react-router'
 import { Login } from './login'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { createBrowserHistory } from 'history'
